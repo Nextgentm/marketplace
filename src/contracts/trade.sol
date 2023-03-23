6fa12c64cd4fcb8321c8becee5c5b793e41bb533
Mahesh <mahesh@Maheshs-MacBook-Pro.local>
2023-03-23 19:33:33 +0530

    Trade contract

---
+++ src/contracts/trade.sol
@@ -0,0 +1,209 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.14;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/interfaces/IERC2981.sol";
+import "./interface/ITransferProxy.sol";
+
+contract Trade is AccessControl {
+    enum BuyingAssetType {
+        ERC1155,
+        ERC721
+    }
+
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+    event SellerFee(uint8 sellerFee);
+    event BuyerFee(uint8 buyerFee);
+    event BuyAsset(
+        address indexed assetOwner,
+        uint256 indexed tokenId,
+        uint256 quantity,
+        address indexed buyer
+    );
+    event ExecuteBid(
+        address indexed assetOwner,
+        uint256 indexed tokenId,
+        uint256 quantity,
+        address indexed buyer
+    );
+    // buyer platformFee
+    uint8 private buyerFeePermille;
+    //seller platformFee
+    uint8 private sellerFeePermille;
+    ITransferProxy public transferProxy;
+    //contract owner
+    address public owner;
+
+    address public signer;
+
+    mapping(uint256 => bool) private usedNonce;
+
+    // Create a new role identifier for the minter role
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
+    /** Fee Struct
+        @param platformFee  uint256 (buyerFee + sellerFee) value which is transferred to current contract owner.
+        @param assetFee  uint256  assetvalue which is transferred to current seller of the NFT.
+        @param royaltyFee  uint256 value, transferred to Minter of the NFT.
+        @param price  uint256 value, the combination of buyerFee and assetValue.
+        @param tokenCreator address value, it's store the creator of NFT.
+     */
+    struct Fee {
+        uint256 platformFee;
+        uint256 assetFee;
+        uint256 royaltyFee;
+        uint256 price;
+        address tokenCreator;
+    }
+
+    
+    /** Order Params
+        @param seller address of user,who's selling the NFT.
+        @param buyer address of user, who's buying the NFT.
+        @param erc20Address address of the token, which is used as payment token(WETH/WBNB/WMATIC...)
+        @param nftAddress address of NFT contract where the NFT token is created/Minted.
+        @param nftType an enum value, if the type is ERC721/ERC1155 the enum value is 0/1.
+        @param uintprice the Price Each NFT it's not including the buyerFee.
+        @param amout the price of NFT(assetFee + buyerFee).
+        @param tokenId 
+        @param qty number of quantity to be transfer.
+     */
+    struct Order {
+        address seller;
+        address buyer;
+        address erc20Address;
+        address nftAddress;
+        BuyingAssetType nftType;
+        uint256 unitPrice;
+        bool skipRoyalty;
+        uint256 amount;
+        uint256 tokenId;
+        string tokenURI;
+        uint256 supply;
+        uint96 royaltyFee;
+        uint256 qty;
+    }
+
+    constructor(
+        uint8 _buyerFee,
+        uint8 _sellerFee,
+        ITransferProxy _transferProxy
+    ) {
+        buyerFeePermille = _buyerFee;
+        sellerFeePermille = _sellerFee;
+        transferProxy = _transferProxy;
+        owner = msg.sender;
+        signer = msg.sender;
+        _setupRole(ADMIN_ROLE, msg.sender);
+    }
+
+    /**
+        returns the buyerservice Fee in multiply of 1000.
+     */
+
+    function buyerServiceFee() external view virtual returns (uint8) {
+        return buyerFeePermille;
+    }
+
+    /**
+        returns the sellerservice Fee in multiply of 1000.
+     */
+
+    function sellerServiceFee() external view virtual returns (uint8) {
+        return sellerFeePermille;
+    }
+
+    /** 
+        @param _buyerFee  value for buyerservice in multiply of 1000.
+    */
+
+    function setBuyerServiceFee(uint8 _buyerFee)
+        external
+        onlyRole(ADMIN_ROLE)
+        returns (bool)
+    {
+        buyerFeePermille = _buyerFee;
+        emit BuyerFee(buyerFeePermille);
+        return true;
+    }
+
+    /** 
+        @param _sellerFee  value for buyerservice in multiply of 1000.
+    */
+
+    function setSellerServiceFee(uint8 _sellerFee)
+        external
+        onlyRole(ADMIN_ROLE)
+        returns (bool)
+    {
+        sellerFeePermille = _sellerFee;
+        emit SellerFee(sellerFeePermille);
+        return true;
+    }
+
+    /**
+        transfers the contract ownership to newowner address.    
+        @param newOwner address of newOwner
+     */
+
+    function transferOwnership(address newOwner)
+        external
+        onlyRole(ADMIN_ROLE)
+        returns (bool)
+    {
+        require(
+            newOwner != address(0),
+            "Ownable: new owner is the zero address"
+        );
+        _revokeRole(ADMIN_ROLE, owner);
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+        _setupRole(ADMIN_ROLE, newOwner);
+        return true;
+    }
+
+    /**
+        excuting the NFT order.
+        @param order ordervalues(seller, buyer,...).
+        @param sign Sign value(v, r, f).
+    */
+
+    function buyAsset(Order calldata order)
+        external
+        returns (bool)
+    {
+        Fee memory fee = getFees(
+            order
+        );
+        require(
+            (fee.price >= order.unitPrice * order.qty),
+            "Paid invalid amount"
+        );
+        address buyer = msg.sender;
+        tradeAsset(order, fee, buyer, order.seller);
+        emit BuyAsset(order.seller, order.tokenId, order.qty, msg.sender);
+        return true;
+    }
+
+    /**
+        excuting the NFT order.
+        @param order ordervalues(seller, buyer,...).
+    */
+
+    function executeBid(Order calldata order)
+        external
+        returns (bool)
+    {
+        Fee memory fee = getFees(
+            order
+        );
+        address seller = msg.sender;
+        tradeAsset(order, fee, order.buyer, seller);
+        emit ExecuteBid(msg.sender, order.tokenId, order.qty, order.buyer);
+        return true;
+    }
+
+}
