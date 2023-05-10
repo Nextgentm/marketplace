892231a1d06c769c1a8d7517367bb40a42dee3d5
MayureshKhemnar <mayuresh.tas@gmail.com>
2023-05-10 15:15:22 +0530

    Testing Fixes
    - Show properties on create page.
    - Page reloading
    - Onsuccessful auction creation redirect to new path.
    - Inside Live sale show "buy now" for only live auctions
    - Accept bid part is fixed.

--- src/components/product-details/bid-tab/history-tab-content.jsx
+++ src/components/product-details/bid-tab/history-tab-content.jsx
@@ -2,9 +2,12 @@ import PropTypes from "prop-types";
 import { IDType, ImageType } from "@utils/types";
 import { Table } from "react-bootstrap";
 import { walletAddressShortForm, transactionHashShortForm } from "../../../utils/blockchain";
+import { useContext } from "react";
+import { AppData } from "src/context/app-context";
 
 const HistoryTabContent = ({ history }) => {
 
+  const { walletData, setWalletData } = useContext(AppData);
   return (
     <div className="history-table pt-4">
       <Table striped responsive variant="dark">
@@ -22,8 +25,8 @@ const HistoryTabContent = ({ history }) => {
           {history?.map((item, index) => (
             <tr key={index}>
               <td>{item?.event}</td>
-              <td>{walletAddressShortForm(item?.fromWalletAddress)}</td>
-              <td>{walletAddressShortForm(item?.toWalletAddress)}</td>
+              <td>{walletData.account == item?.fromWalletAddress ? "You" : walletAddressShortForm(item?.fromWalletAddress)}</td>
+              <td>{walletData.account == item?.toWalletAddress ? "You" : walletAddressShortForm(item?.toWalletAddress)}</td>
               <td>{item?.quantity}</td>
               <td>{transactionHashShortForm(item?.transactionHash)}</td>
               {/* <td>{item?.createdAt}</td> */}
