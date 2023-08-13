3023b619b93914d965ae0cf109f880707e23adb0
MayureshKhemnar <mayuresh.tas@gmail.com>
2023-08-13 21:50:23 +0530

    truffle and contract abi update

--- src/contracts/Stacking.sol
+++ src/contracts/Stacking.sol
@@ -10,9 +10,15 @@ import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 
-contract StakingNFT is PausableUpgradeable, AccessControlUpgradeable, UUPSUpgradeable, ReentrancyGuard, ERC1155Holder {
+contract StakingNFT is
+  PausableUpgradeable,
+  AccessControlUpgradeable,
+  UUPSUpgradeable,
+  ReentrancyGuardUpgradeable,
+  ERC1155Holder
+{
   struct StakingInfo {
     uint256 stakedAmount;
     uint256 oldBalance;
