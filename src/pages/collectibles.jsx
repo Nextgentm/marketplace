9a67ae2e4e10b0a8bedaa4ae58e44916b845e4dc
vishal-lootmogule <vishal.solanki@nextgentm.com>
2024-04-25 14:40:06 +0530

    Digital Collectibles
    Update Name Digital Collectibles

--- src/pages/collectibles.jsx
+++ src/pages/collectibles.jsx
@@ -26,7 +26,7 @@ const Collectibles = ({ dataCollectibles, categoriesolds }) => {
       <SEO pageTitle="Explore Simple" />
       <Header />
       <main id="main-content">
-        <Breadcrumb pageTitle={router.query.collection ? "Explore Collection" : "Explore NFT"} currentPage={router.query.collection ? "Explore Collection" : "Explore NFT"} isCollection={true} />
+        <Breadcrumb pageTitle={router.query.collection ? "Explore Collection" : "Explore Digital Collectible"} currentPage={router.query.collection ? "Explore Collection" : "Explore Digital Collectibles"} isCollection={true} />
         {dataCollectibles && (
           <ExploreProductArea
             data={{
