cf3966ec80e609c6540a3b2609e4e0e7001415d3
rutvikbokadiyafantafeat <164156377+rutvikbokadiyafantafeat@users.noreply.github.com>
2024-09-02 11:14:14 +0530

    add like dislike features

--- src/components/product-details/title.jsx
+++ src/components/product-details/title.jsx
@@ -1,21 +1,54 @@
 import PropTypes from "prop-types";
 import clsx from "clsx";
 import ShareDropdown from "../share-dropdown";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import strapi from "@utils/strapi";
 
-const ProductTitle = ({ className, title, likeCount }) => (
-  <div className={clsx("pd-title-area", className)}>
-    <h4 className="title" style={{ fontFamily: "inherit !important" }}>{title}</h4>
-    <div className="pd-react-area">
-      <div className="heart-count">
-        <i className="feather-heart" />
-        <span>{likeCount}</span>
+const ProductTitle = ({ className, title, likeCount: initialLikeCount, userId, collectibleId }) => {
+
+  const [likeCount, setLikeCount] = useState(initialLikeCount);
+  const [isLiked, setIsLiked] = useState(true);
+
+  const handleLikeClick = async () => {
+    try {
+      if (!userId || !collectibleId) {
+        /**
+         * show popup
+         */
+        toast.error("Please login.");
+        return;
+      }
+
+      let response = await strapi.find("collectible-like", {
+        filters: {
+          userId,
+          collectibleId
+        }
+      });
+      setLikeCount((prevCount) => response?.totalCount ? response?.totalCount : 0);      
+    } catch (error) {
+      console.error("Error liking the product:", error);
+    }
+  };
+
+  return (
+    <>
+      <div className={clsx("pd-title-area", className)}>
+        <h4 className="title" style={{ fontFamily: "inherit !important" }}>{title}</h4>
+        <div className="pd-react-area">
+          <div className="heart-count" onClick={handleLikeClick}>
+            <i className="feather-heart" />
+            <span>{likeCount}</span>
+          </div>
+          <div className="count">
+            <ShareDropdown />
+          </div>
+        </div>
       </div>
-      <div className="count">
-        <ShareDropdown />
-      </div>
-    </div>
-  </div>
-);
+    </>
+  )
+};
 
 ProductTitle.propTypes = {
   className: PropTypes.string,
