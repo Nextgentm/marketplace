86a8875a32e03228598f0362d08762bd1784fb63
rutvikbokadiya <rutvikbokadiya6513@gmail.com>
2025-07-02 14:57:57 +0530

    fix url

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -18,7 +18,7 @@ const SEO = ({ pageTitle, slug, description, keywords }) => {
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       <link rel="icon" href="/favicon.ico" />
       <link rel="canonical" href={canonicalUrl} />
-      {keywords && <meta name="keywords" content={keywords} />}
+      { keywords && <meta name="keywords" content={keywords} /> }
     </Head>
   );
 };
